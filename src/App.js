385aae04bc90e50bf866aeec83bfe2f83d3efdb5
gelai <gelaigdao@gmail.com>
2023-08-31 22:06:46 +0800

    edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AppHeader from "./components/header";
+import AppHero from "./components/hero";
+import AppAbout from "./components/about";
+import AppServices from "./components/services";
+import AppWorks from "./components/works";
+import AppTeams from "./components/teams";
+import AppTestimonials from "./components/testimonials";
+import AppContact from "./components/contact";
+import AppFooter from "./components/footer";
+
 function App() {
-  return <h1>HI!</h1>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header id="header">
+          <AppHeader />
+        </header>
+        <main>
+          <AppHero />
+          <AppAbout />
+          <AppServices />
+          <AppWorks />
+          <AppTeams />
+          <AppTestimonials />
+          <AppContact />
+        </main>
+        <footer id="footer">
+          <AppFooter />
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
